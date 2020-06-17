8f0429f60ad38ac4cf38352c4b8441e7ec5441e2
Adriller Ferraira <a.genova@pitagorinesgroup.com>
2020-06-17 10:18:47 -0300

    fix(analytics): removed queryString call and added screenName to event

--- analytics.js
+++ analytics.js
@@ -1,9 +1,9 @@
-import { Platform, Dimensions } from 'react-native';
-import Constants from 'expo-constants';
+import { Platform, Dimensions } from "react-native";
+import Constants from "expo-constants";
 
-import { ScreenHit, PageHit, Event, Serializable } from './hits';
+import { ScreenHit, PageHit, Event, Serializable } from "./hits";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 let defaultOptions = { debug: false };
 
@@ -16,19 +16,19 @@ const getWebViewUserAgent = async (options) => {
         }
         if (webViewUserAgent) return resolve(webViewUserAgent);
         Constants.getWebViewUserAgentAsync()
-          .then(userAgent => {
-              webViewUserAgent = userAgent;
-              resolve(userAgent);
-          })
-          .catch(() => resolve('unknown user agent'))
+            .then((userAgent) => {
+                webViewUserAgent = userAgent;
+                resolve(userAgent);
+            })
+            .catch(() => resolve("unknown user agent"));
     });
-}
+};
 
 export default class Analytics {
-    customDimensions = []
-    customMetrics = []
+    customDimensions = [];
+    customMetrics = [];
 
-    constructor(propertyId, additionalParameters = {}, options = defaultOptions){
+    constructor(propertyId, additionalParameters = {}, options = defaultOptions) {
         this.propertyId = propertyId;
         this.options = options;
         this.clientId = Constants.deviceId;
@@ -37,109 +37,110 @@ export default class Analytics {
             aid: Constants.manifest.slug,
             av: Constants.manifest.version,
             sr: `${width}x${height}`,
-            ...additionalParameters
+            ...additionalParameters,
         };
 
-        this.promiseGetWebViewUserAgentAsync = getWebViewUserAgent(options)
-            .then(userAgent => {
-                this.userAgent = userAgent;
-                if(this.options.debug){
-                    console.log(`[expo-analytics] UserAgent=${userAgent}`);
-                    console.log(`[expo-analytics] Additional parameters=`, this.parameters);
-                }
-            });
+        this.promiseGetWebViewUserAgentAsync = getWebViewUserAgent(options).then((userAgent) => {
+            this.userAgent = userAgent;
+            if (this.options.debug) {
+                console.log(`[expo-analytics] UserAgent=${userAgent}`);
+                console.log(`[expo-analytics] Additional parameters=`, this.parameters);
+            }
+        });
     }
 
-    hit(hit){
+    hit(hit) {
         // send only after the user agent is saved
-        return this.promiseGetWebViewUserAgentAsync
-            .then(() => this.send(hit));
+        return this.promiseGetWebViewUserAgentAsync.then(() => this.send(hit));
     }
 
-    event(event){
+    event(event) {
         // send only after the user agent is saved
-        return this.promiseGetWebViewUserAgentAsync
-            .then(() => this.send(event));
+        return this.promiseGetWebViewUserAgentAsync.then(() => this.send(event));
     }
 
-    addParameter(name, value){
+    addParameter(name, value) {
         this.parameters[name] = value;
     }
 
-    addCustomDimension(index, value){
+    addCustomDimension(index, value) {
         this.customDimensions[index] = value;
     }
 
-    removeCustomDimension(index){
+    removeCustomDimension(index) {
         delete this.customDimensions[index];
     }
 
     addCustomMetric(index, value) {
         this.customMetrics[index] = value;
-      }
+    }
 
     removeCustomMetric(index) {
         delete this.customMetrics[index];
     }
 
     send(hit) {
-        /* format: https://www.google-analytics.com/collect? +
-        * &tid= GA property ID (required)
-        * &v= GA protocol version (always 1) (required)
-        * &t= hit type (pageview / screenview)
-        * &dp= page name (if hit type is pageview)
-        * &cd= screen name (if hit type is screenview)
-        * &cid= anonymous client ID (optional if uid is given)
-        * &uid= user id (optional if cid is given)
-        * &ua= user agent override
-        * &an= app name (required for any of the other app parameters to work)
-        * &aid= app id
-        * &av= app version
-        * &sr= screen resolution
-        * &cd{n}= custom dimensions
-        * &cm{n}= custom metrics
-        * &z= cache buster (prevent browsers from caching GET requests -- should always be last)
-        * 
-        * Ecommerce track support (transaction)
-        * &ti= transaction The transaction ID. (e.g. 1234)
-        * &ta= The store or affiliation from which this transaction occurred (e.g. Acme Clothing).
-        * &tr= Specifies the total revenue or grand total associated with the transaction (e.g. 11.99). This value may include shipping, tax costs, or other adjustments to total revenue that you want to include as part of your revenue calculations.
-        * &tt= Specifies the total shipping cost of the transaction. (e.g. 5)
-        * 
-        * Ecommerce track support (addItem)
-        * &ti= transaction The transaction ID. (e.g. 1234)
-        * &in= The item name. (e.g. Fluffy Pink Bunnies)
-        * &ip= The individual, unit, price for each item. (e.g. 11.99)
-        * &iq= The number of units purchased in the transaction. If a non-integer value is passed into this field (e.g. 1.5), it will be rounded to the closest integer value.
-        * &ic= TSpecifies the SKU or item code. (e.g. SKU47)
-        * &iv= The category to which the item belongs (e.g. Party Toys)
-        */
+        console.log("hit", hit);
+        console.log("params", params);
 
-        const customDimensions = this.customDimensions.map((value, index) => `cd${index}=${value}`).join('&');
-        const customMetrics = this.customMetrics.map((value, index) => `cm${index}=${value}`).join('&');
+        /* format: https://www.google-analytics.com/collect? +
+         * &tid= GA property ID (required)
+         * &v= GA protocol version (always 1) (required)
+         * &t= hit type (pageview / screenview)
+         * &dp= page name (if hit type is pageview)
+         * &cd= screen name (if hit type is screenview)
+         * &cid= anonymous client ID (optional if uid is given)
+         * &uid= user id (optional if cid is given)
+         * &ua= user agent override
+         * &an= app name (required for any of the other app parameters to work)
+         * &aid= app id
+         * &av= app version
+         * &sr= screen resolution
+         * &cd{n}= custom dimensions
+         * &cm{n}= custom metrics
+         * &z= cache buster (prevent browsers from caching GET requests -- should always be last)
+         *
+         * Ecommerce track support (transaction)
+         * &ti= transaction The transaction ID. (e.g. 1234)
+         * &ta= The store or affiliation from which this transaction occurred (e.g. Acme Clothing).
+         * &tr= Specifies the total revenue or grand total associated with the transaction (e.g. 11.99). This value may include shipping, tax costs, or other adjustments to total revenue that you want to include as part of your revenue calculations.
+         * &tt= Specifies the total shipping cost of the transaction. (e.g. 5)
+         *
+         * Ecommerce track support (addItem)
+         * &ti= transaction The transaction ID. (e.g. 1234)
+         * &in= The item name. (e.g. Fluffy Pink Bunnies)
+         * &ip= The individual, unit, price for each item. (e.g. 11.99)
+         * &iq= The number of units purchased in the transaction. If a non-integer value is passed into this field (e.g. 1.5), it will be rounded to the closest integer value.
+         * &ic= TSpecifies the SKU or item code. (e.g. SKU47)
+         * &iv= The category to which the item belongs (e.g. Party Toys)
+         */
+
+        const customDimensions = this.customDimensions.map((value, index) => `cd${index}=${value}`).join("&");
+        const customMetrics = this.customMetrics.map((value, index) => `cm${index}=${value}`).join("&");
 
         const params = new Serializable(this.parameters).toQueryString();
 
-        const url = `https://www.google-analytics.com/collect?tid=${this.propertyId}&v=1&cid=${this.clientId}&${hit.toQueryString()}&${params}&${customDimensions}&${customMetrics}&z=${Math.round(Math.random() * 1e8)}`;
+        const url = `https://www.google-analytics.com/collect?tid=${this.propertyId}&v=1&cid=${this.clientId}&${hit.toQueryString()}&${params}&${customDimensions}&${customMetrics}&z=${Math.round(
+            Math.random() * 1e8
+        )}`;
 
         //Keep original options if on mobile
         let options = {
-            method: 'get',
+            method: "get",
             headers: {
-                'User-Agent': this.userAgent,
-            }
+                "User-Agent": this.userAgent,
+            },
         };
 
-        if (Platform.OS === 'web') {
+        if (Platform.OS === "web") {
             //Request opaque resources to avoid preflight CORS error in Safari
-            options.mode = 'no-cors';
+            options.mode = "no-cors";
         }
 
-        if(this.options.debug){
+        if (this.options.debug) {
             console.log(`[expo-analytics] Sending GET request to ${url}`);
         }
 
         return fetch(url, options);
     }
-
 }
--- hits.js
+++ hits.js
@@ -1,5 +1,4 @@
 export class Serializable {
-
     constructor(props) {
         this.properties = props || {};
     }
@@ -21,49 +20,47 @@ export class Serializable {
         var obj = this.toObject();
         for (var p in obj) {
             if (obj.hasOwnProperty(p) && obj[p]) {
-                str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                str.push(encodeURIComponent(p) + "=" + obj[p]);
             }
         }
-        return str.join('&');
+        return str.join("&");
     }
 }
 
 class Hit extends Serializable {
-    
-    sent = false
+    sent = false;
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 }
 
-
 export class PageHit extends Hit {
     constructor(screenName, screenTitle) {
-        super({ dt: screenName, dp: screenTitle || screenName, t: 'pageview' });
+        super({ dt: screenName, dp: screenTitle || screenName, t: "pageview" });
     }
 }
 
 export class ScreenHit extends Hit {
     constructor(screenName) {
-        super({ cd: screenName, t: 'screenview' });
+        super({ cd: screenName, t: "screenview" });
     }
 }
 
 export class Event extends Hit {
-    constructor(category, action, label, value) {
-        super({ ec: category, ea: action, el: label, ev: value, t: 'event' });
+    constructor(category, action, label, value, screenName) {
+        super({ ec: category, ea: action, el: label, ev: value, t: "event", dt: screenName });
     }
 }
 
 export class Transaction extends Hit {
     constructor(id, affiliation, revenue, shipping, tax) {
-        super({ ti: id, ta: affiliation, tr: revenue, tt: tax, t: 'transaction' });
+        super({ ti: id, ta: affiliation, tr: revenue, tt: tax, t: "transaction" });
     }
 }
 
 export class AddItem extends Hit {
     constructor(id, name, price, quantity, sku, category) {
-        super({ti: id, in: name, ip: price, iq: quantity, ic: sku, iv: category, t: 'item' });
+        super({ ti: id, in: name, ip: price, iq: quantity, ic: sku, iv: category, t: "item" });
     }
 }
